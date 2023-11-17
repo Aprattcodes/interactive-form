b8bc42f6b20cd3864d51a5427e3173dd14dad739
Alyssa Pratt <alyssakpratt@gmail.com>
2023-11-16 19:42:39 -0800

    makes changes so that certain elements react on page load

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const nameField = document.querySelector("#name");
+const otherJobInput = document.querySelector("#other-job-role");
+
+window.addEventListener("load", (event) => { //on page load -
+    nameField.focus(); //name field is focused
+    otherJobInput.setAttribute("type","hidden"); //sets other job input not visible by default
+  });
+
+  
